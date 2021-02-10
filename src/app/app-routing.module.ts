0b501d38a1ea4b0728ca236592185e897ade4e84
Cynthia Oka <cynthia.mosilva@gmail.com>
2021-02-10 15:38:03 -0300

    commit prova

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'blocks',
+    component: BlocksComponent
+  },
+  {
+    path: 'cards',
+    component: CardsComponent
+  },
+  {
+    path: 'forms',
+    component: FormsComponent
+  },
+  {
+    path: 'pricing',
+    component: PricingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent,
+    BlocksComponent,
+    CardsComponent,
+    FormsComponent,
+    HomeComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
